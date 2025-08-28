f8d0dc494db1f9d84dfd3a18b27d0a527f487510
Alamin Islam <alamin648890@gmail.com>
2025-08-28 23:00:02 +0600

    added calling feature also call history and clear history feature

--- js/main.js
+++ js/main.js
@@ -11,3 +11,45 @@ heartIcon.forEach((btn) => {
     heartNumber.textContent = heartCount;
   });
 });
+
+// For calling functionality
+
+let coins = 100;
+
+const coinsCount = document.getElementById("coins");
+const callHistoryList = document.getElementById("call-history");
+
+function makeCall(serviceName, serviceNumber) {
+  if (coins < 20) {
+    alert("Not enough coins to make a call!");
+    return;
+  }
+
+  coins -= 20;
+
+  coinsCount.textContent = coins;
+  alert("Calling " + serviceName + " at " + serviceNumber);
+
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+
+  // Create history card
+  const div = document.createElement("div");
+  div.className = "pt-3";
+
+  div.innerHTML = `
+  <div class="max-w-sm bg-[#FAFAFB] rounded-lg  px-4 py-3 flex justify-between items-center">
+      <div>
+        <h2 class="text-sm font-medium text-gray-900">${serviceName}</h2>
+        <p class="text-xs text-gray-500">${serviceNumber}</p>
+      </div>
+      <div class="text-xs text-gray-500">${time}</div>
+    </div>
+      `;
+
+  callHistoryList.appendChild(div);
+}
+
+function clearHistory() {
+  callHistoryList.innerHTML = ""; 
+}
